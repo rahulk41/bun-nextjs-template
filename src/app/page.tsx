7b5172d1f1b24546838539cff8e044f12abb3433
hellboy41785 <unknown41785@gmail.com>
2025-08-19 14:08:31 +0530

    feat: initinal

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Home() {
+  return (
+    <div className="max-w-6xl w-full mx-auto flex flex-col justify-between items-center p-8">
+      <p className="text-4xl">Hello World !</p>
+    </div>
+  );
+}
---
+++ src/provider/tanstack.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+interface Props {
+  children: ReactNode;
+}
+
+const TanStackProvider = ({ children }: Props) => {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default TanStackProvider;
